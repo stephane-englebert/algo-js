b2af0001df006fe4d1e37ed652ee4eead62d3a8a
stephane-englebert <74079227+stephane-englebert@users.noreply.github.com>
2021-01-18 16:39:47 +0100

    first commit

---
+++ ex-1-2.js
@@ -0,0 +1,7 @@
+// MISSION
+// Define three variables: name, firstName and city. 
+// Display them like this: "Your name is Gerard Lambert and you live in Paris.".
+let userName = "Fischer";
+let userFirstName = "Bobby";
+let userCity = "Reykjavik";
+console.log("Your name is " + userFirstName + " " + userName + " and you live in " + userCity);
